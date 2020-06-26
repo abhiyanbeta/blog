cd08e0085f7356d37f89667e410c10ea379368b9
abhiyanbeta <abhiyanbeta@rocketmail.com>
2020-06-26 20:53:44 +0100

    update

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 OUTDIR=docs
 LOCALHOST="http://localhost:2368/"
 wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCALHOST
+git status
 git add .
+git status
 git commit -m "site updated"
 git push
+git status
